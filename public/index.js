dfc9c4012575a14b6f25b19ee9276f9ad7394d87
Andro Slodoban Ostoic Munoz <aostoic@conicyt.cl>
2019-06-20 17:00:50 -0400

    arreglo

--- public/index.js
+++ public/index.js
@@ -31,3 +31,94 @@ function enviarMensaje() {}
 function ModalTelefonos() {
   llamar.click();
 }
+
+console.log("hooola");
+
+var estatica = false;
+var estatica2 = false;
+var estatica3 = false;
+
+$(window).on("scroll", function() {
+  var section1 = document.getElementById("section1");
+  var quienesSomos = document.getElementById("quienesSomos");
+  var servicios = document.getElementById("servicios");
+
+  var section1 = esVisible(section1);
+
+  console.log(section1);
+
+  if (section1 && !estatica) {
+    estatica = true;
+    ponerAnimacion();
+    setTimeout("sacarAnimacion();", 5000);
+  }
+
+  var quienesSomos = esVisible(quienesSomos);
+
+  if (quienesSomos && !estatica2) {
+    estatica2 = true;
+    $("#hTextQuienesSomos").addClass("animated infinite bounceInLeft delay-1s");
+    setTimeout(
+      ' $("#hTextQuienesSomos").removeClass("animated infinite bounceInLeft delay-1s");',
+      2000
+    );
+  }
+  var servicios = esVisible(servicios);
+
+  if (servicios && !estatica3) {
+    estatica3 = true;
+    $("#hTextServicios").addClass("animated infinite bounceInLeft delay-1s");
+    setTimeout(
+      ' $("#hTextServicios").removeClass("animated infinite bounceInLeft delay-1s");',
+      2000
+    );
+  }
+
+  if (section1 == false) {
+    estatica = false;
+  }
+  if (quienesSomos == false) {
+    estatica2 = false;
+  }
+  if (servicios == false) {
+    estatica3 = false;
+  }
+});
+
+function ponerAnimacion() {
+  $("#iconUbicacion").addClass("animated infinite bounce delay-1s");
+  $("#iconLlamar").addClass("animated infinite bounce delay-1s");
+  $("#iconMensaje").addClass("animated infinite bounce delay-1s");
+}
+
+function sacarAnimacion() {
+  console.log("para");
+  $("#iconUbicacion").removeClass("animated infinite bounce delay-1s");
+  $("#iconLlamar").removeClass("animated infinite bounce delay-1s");
+  $("#iconMensaje").removeClass("animated infinite bounce delay-1s");
+}
+
+function esVisible(elem) {
+  /* Ventana de Visualización*/
+  var posTopView = $(window).scrollTop();
+  var posButView = posTopView + $(window).height();
+  /* Elemento a validar*/
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+  /* Comparamos los dos valores tanto del elemento como de la ventana*/
+  return (
+    (elemBottom < posButView && elemBottom > posTopView) ||
+    (elemTop > posTopView && elemTop < posButView)
+  );
+}
+
+var servicios = document.getElementById("servicios");
+var servicios = esVisible(servicios);
+if (servicios && !estatica3) {
+  estatica3 = true;
+  $("#hTextServicios").addClass("animated infinite bounceInLeft delay-1s");
+  setTimeout(
+    ' $("#hTextServicios").removeClass("animated infinite bounceInLeft delay-1s");',
+    3000
+  );
+}
